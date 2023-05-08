9872473ae81bb2c7ef7467740fc1b2a2ee654600
Aiperi <you@edilbekova_aiperi.mail.ru>
2023-05-08 19:19:53 +0300

    first commit

---
+++ account/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ThermalUnderwearConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'account'
---
+++ orders/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import *
+
+
+# class SubscriberForm(forms.ModelForm):
+#
+#     class Meta:
+#         model = Subscriber
+#         exclude = [""]
+#         # fields = ('email',)
+#         # exclude = ('some_field',)
+
+
+
